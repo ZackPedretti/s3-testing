503fb4d9db748e1807827ebb49ba08c61316bed3
ZackPedretti <zackpedretti52@gmail.com>
2025-11-03 09:51:42 +0100

    feat: Gin API is complete

--- gin/entities.go
+++ gin/entities.go
@@ -1,7 +1,17 @@
 package main
 
+import "fmt"
+
 type Song struct {
 	Artist    string  `form:"artist" binding:"required"`
 	Name      string  `form:"song" binding:"required"`
 	Extension *string `form:"ext"`
+}
+
+func (s Song) BuildFileName() string {
+	return fmt.Sprintf("%s.%s", s.Name, *s.Extension)
+}
+
+func (s Song) BuildKey() string {
+	return fmt.Sprintf("%s/%s.%s", s.Artist, s.Name, *s.Extension)
 }
--- gin/s3_connector.go
+++ gin/s3_connector.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"io"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/gin-gonic/gin"
@@ -23,9 +26,31 @@ func initS3Client() (*s3.Client, error) {
 }
 
 func putSongToS3(ctx *gin.Context, file []byte, song Song, s3Client *s3.Client) error {
+	_, err := s3Client.PutObject(ctx, &s3.PutObjectInput{
+		Bucket: aws.String("jukebox"),
+		Key:    aws.String(song.BuildKey()),
+		Body:   bytes.NewReader(file),
+	})
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
 func getSongFromS3(ctx *gin.Context, song Song, s3Client *s3.Client) ([]byte, error) {
-	return nil, nil
+	result, err := s3Client.GetObject(ctx, &s3.GetObjectInput{
+		Bucket: aws.String("jukebox"),
+		Key:    aws.String(song.BuildKey()),
+	})
+	if err != nil {
+		return nil, err
+	}
+	defer result.Body.Close()
+
+	file, err := io.ReadAll(result.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return file, nil
 }
